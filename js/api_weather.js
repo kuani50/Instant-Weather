17c98dc8e44017e7e90c898d33dd1a4a4143fbea
Léo-Paul <marie2328@unicaen.fr>
2024-10-10 09:02:22 +0200

    :bug: Null value added to weather code table & label added

--- js/api_weather.js
+++ js/api_weather.js
@@ -2,34 +2,20 @@ async function weather(cityCode){
 
     let reponse = await fetch("https://api.meteo-concept.com/api/forecast/daily?token=6de9e5283b4e827ab2a94e0b552c8c74a8a243d900f00ccfaa329f63ec8fa42a&insee=" + cityCode);
     let tab = await reponse.json();
-
-    console.log(tab);
-    console.log("weather : " + tab[0])
-
-}
-
-async function weather2(){
-
-    let reponse = await fetch("../tempores.json");
-    let tab = await reponse.json();
-    let weatherTab = []; 
     
-    console.log(tab);
+    let weatherTab = []; 
 
-
-    tab.forecast.forEach(element =>{
-        console.log(element["weather"]);
+    tab.forecast.forEach(async element =>{
         weatherTab.push({
             "latitude" : element["latitude"], "longitude" : element["longitude"],
             "tMax" : element["tmax"], "tMin" : element["tmin"], "probaRain" : element["probarain"],
             "sunHours" : element["sun_hours"], "cumulRain" : element["rr10"], "avgWind" : element["wind10m"],
-            "windDir" : element["dirwind10m"]
+            "windDir" : element["dirwind10m"], "weather" : (await getCodeWeather(element["weather"])),
+            "weatherCode" : element["weather"]
         });
     });
-
-    console.table(weatherTab);
+    
     return weatherTab;
-
 }
 
 async function loadCodeWeather(){
