5aba6430d44602b6cd11e006cbbcede91c804faf
Léo-Paul <marie2328@unicaen.fr>
2024-10-17 17:47:01 +0200

    :sparkles: Add day selection in settings and save in the LocalStorage

--- js/settings.js
+++ js/settings.js
@@ -8,12 +8,24 @@ let latitudeAndLongitude = document.getElementById("latitudeAndLongitude");
 let rain = document.getElementById("rain");
 let windSpeed = document.getElementById("windSpeed");
 let windDirection = document.getElementById("windDirection");
+let sliderForNumberOfDay = document.getElementById("sliderNbDays");
+let textNumberOfDay = document.getElementById("textNumberOfDays");
 
 
 if(strToBool(localStorage.getItem("latitudeAndLongitude"))) latitudeAndLongitude.checked = true;
 if(strToBool(localStorage.getItem("rain"))) rain.checked = true;
 if(strToBool(localStorage.getItem("windSpeed"))) windSpeed.checked = true;
 if(strToBool(localStorage.getItem("windDirection"))) windDirection.checked = true;
+
+if(localStorage.getItem("nbDays") != 1){
+    sliderForNumberOfDay.value = localStorage.getItem("nbDays");
+    textNumberOfDay.innerText = localStorage.getItem("nbDays") + " jours";
+}
+
+sliderForNumberOfDay.addEventListener('change', ()=>{
+    localStorage.setItem("nbDays", sliderForNumberOfDay.value);
+    textNumberOfDay.innerText = sliderForNumberOfDay.value + " jours";
+});
 
 latitudeAndLongitude.addEventListener('change', () => {
     localStorage.setItem("latitudeAndLongitude", latitudeAndLongitude.checked);
