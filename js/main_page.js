17aa24314c1e4b820550848cf5a81145b66db158
kuani <116524310+kuani50@users.noreply.github.com>
2024-10-17 14:53:16 +0200

    :bug: Load localStorage on settings page

--- js/main_page.js
+++ js/main_page.js
@@ -1,48 +1,24 @@
 import { search } from "./api_postal_code.js";
 
-let settings_button = document.getElementById('settings_button');
-let settings_page = document.getElementById('settings_page');
+const settings_button = document.getElementById('settings_button');
+const settings_page = document.getElementById('settings_page');
 
-let settings_object = document.getElementById('settings_object');
+const settings_object = document.getElementById('settings_object');
 
-let search_bar = document.getElementById('search_bar');
-let city_choice = document.getElementById('city_choice');
+const search_bar = document.getElementById('search_bar');
+const city_choice = document.getElementById('city_choice');
 const row_template = document.getElementById('row_template');
 
 setupLocalStorage();
 
-// will manage everything related to the settings only when the settings page is open
-settings_object.addEventListener('load', () => {
 
-    // manages the closing button
-    let close = settings_object.contentDocument.getElementById('close');
-    
+settings_object.addEventListener('load', () => {
+    const close = settings_object.contentDocument.getElementById('close');
     close.addEventListener('click', () => {
         settings_page.classList.toggle('hidden');
     });
-
-    // puts the settings choice in the local storage
-    let latitudeAndLongitude = settings_object.contentDocument.getElementById("latitudeAndLongitude");
-    let rain = settings_object.contentDocument.getElementById("rain");
-    let windSpeed = settings_object.contentDocument.getElementById("windSpeed");
-    let windDirection = settings_object.contentDocument.getElementById("windDirection");
-
-    latitudeAndLongitude.addEventListener('change', () => {
-        localStorage.setItem("latitudeAndLongitude", latitudeAndLongitude.checked);
-    });
-
-    rain.addEventListener('change', () => {
-        localStorage.setItem("rain", rain.checked);
-    });
-
-    windSpeed.addEventListener('change', () => {
-        localStorage.setItem("windSpeed", windSpeed.checked);
-    });
-
-    windDirection.addEventListener('change', () => {
-        localStorage.setItem("windDirection", windDirection.checked);
-    });
 });
+
 
 // manages the button to open the settings
 settings_button.addEventListener('click', () => {
