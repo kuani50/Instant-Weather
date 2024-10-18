4e59d7b8d27e20fc2e38980b3e843f427f2be150
Isaac <isaac.delauney@etu.unicaen.fr>
2024-10-18 19:49:57 +0200

    💡 Fixed some comments in api_weather.js

--- js/api_weather.js
+++ js/api_weather.js
@@ -1,7 +1,8 @@
+// Fetches the weather data from the API with the city code chosen by the user
 export async function weather(cityCode){
 
-    let reponse = await fetch("https://api.meteo-concept.com/api/forecast/daily?token=6de9e5283b4e827ab2a94e0b552c8c74a8a243d900f00ccfaa329f63ec8fa42a&insee=" + cityCode);
-    let tab = await reponse.json();
+    let reply = await fetch("https://api.meteo-concept.com/api/forecast/daily?token=6de9e5283b4e827ab2a94e0b552c8c74a8a243d900f00ccfaa329f63ec8fa42a&insee=" + cityCode);
+    let tab = await reply.json();
     
     let weatherTab = []; 
 
@@ -18,15 +19,17 @@ export async function weather(cityCode){
     return weatherTab;
 }
 
+// Loads the json file containing the weather codes
 async function loadCodeWeather(){
     return await (await fetch("../assets/code_weather.json")).json();
 }
 
+// Returns the weather associated with the code
 async function getCodeWeather(code){
     return (await loadCodeWeather())[code];
 }
 
-
+// Loads the file containing a result of the API
 export async function weather2(cityCode){
 
     let reponse = await fetch("/tempores.json");
