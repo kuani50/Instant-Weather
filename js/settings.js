ecceb8887643532a1593c5a64ae56cc5aa258c59
kuani <116524310+kuani50@users.noreply.github.com>
2024-10-19 16:01:32 +0200

    :bug: Fix slider label in settings

--- js/settings.js
+++ js/settings.js
@@ -15,17 +15,23 @@ if(strToBool(localStorage.getItem("rain"))) rain.checked = true;
 if(strToBool(localStorage.getItem("windSpeed"))) windSpeed.checked = true;
 if(strToBool(localStorage.getItem("windDirection"))) windDirection.checked = true;
 
+// Display slider label
+function updateSliderLabel(){
+    textNumberOfDay.innerText = `${sliderForNumberOfDay.value} jour${sliderForNumberOfDay.value>1 ? 's':''}`;
+}
+
 // Coordinates the slider with the local storage
 if(localStorage.getItem("nbDays") != undefined){
     sliderForNumberOfDay.value = localStorage.getItem("nbDays");
-    textNumberOfDay.innerText = localStorage.getItem("nbDays") + " jours";
+    updateSliderLabel();
 }
 
 // Updates the local storage when the slider is moved
 sliderForNumberOfDay.addEventListener('input', ()=>{
     localStorage.setItem("nbDays", sliderForNumberOfDay.value);
-    textNumberOfDay.innerText = sliderForNumberOfDay.value + " jours";
+    updateSliderLabel();
 });
+
 
 // Updates the local storage when the latitude and longitude button is clicked
 latitudeAndLongitude.addEventListener('change', () => {
